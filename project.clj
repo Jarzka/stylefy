71edfff2028d8ea8aaa8a224f6998cd3fa54305b
Jari Hanhela <jarihanhela@gmail.com>
2017-05-17 18:13:16 +0300

    First commit of a mysterious library

---
+++ project.clj
@@ -0,0 +1,19 @@
+(defproject stylefy "0.1-alpha1"
+  :description "Library for styling UI components"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.542"]]
+  :plugins [[lein-cljsbuild "1.1.2"]
+            [lein-doo "0.1.7"]]
+  :cljsbuild {:builds [{:id "test"
+                        :source-paths ["src" "test/cljs"]
+                        :compiler {:output-to "target/cljs/test/test.js"
+                                   :output-dir "target/cljs/test"
+                                   :optimizations :none
+                                   :pretty-print true
+                                   :source-map true
+                                   :main stylefy.runner}}
+                       {:id "prod"
+                        :source-paths ["src/cljs"]
+                        :compiler {:output-to "stylefy.js"
+                                   :optimizations :advanced}}]}
+  :profiles {:dev {:dependencies [[reagent "0.6.0-rc"]]}})
---
+++ src/cljs/stylefy/main.cljs
@@ -0,0 +1,1 @@
+(ns stylefy.main)
