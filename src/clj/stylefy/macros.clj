8af0dca1a2280628cc9a286de75b25a9880348d1
Jari Hanhela <jarihanhela@gmail.com>
2017-05-17 20:00:21 +0300

    Fix typo in function call

--- src/clj/stylefy/macros.clj
+++ src/clj/stylefy/macros.clj
@@ -2,7 +2,7 @@
   (:require [garden.core :refer [css]]))
 
 (defmacro style
-  ([style] (defstyle style {}))
+  ([style] (style style {}))
   ([style options]
    (let [class-name (str "__stylefy__" (gensym))
          css-code (css style)]
