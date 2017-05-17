848fc3a0d3ea6bb915f934540bdb2ca459c6db90
Jari Hanhela <jarihanhela@gmail.com>
2017-05-17 20:30:38 +0300

    Fix bad argument name

--- src/stylefy/macros.clj
+++ src/stylefy/macros.clj
@@ -2,9 +2,9 @@
   (:require [garden.core :refer [css]]))
 
 (defmacro style
-  ([style] (style style {}))
-  ([style options]
-   (let [class-name (str "__stylefy__" (gensym))
+  ([style-map] (style style-map {}))
+  ([style-map options]
+    (let [class-name (str "__stylefy__" (gensym))
          css-code (css style)]
      (with-open [writer (clojure.java.io/writer "resources/public/css/stylefy.css" :append true)]
        (.write writer css-code))
