33653c80b6a5dc7df86cb27f0f85f62b346dbdc9
Jari Hanhela <jarihanhela@gmail.com>
2017-05-17 19:03:18 +0300

    Remove useless file

--- examples/src/cljs/stylefy/examples.cljs
+++
@@ -1,8 +0,0 @@
-(ns stylefy.examples
-  (:require [reagent.core :as r]))
-
-(defn- hello-world []
-  [:div "Hello world"])
-
-(defn- main []
-  (r/render hello-world (.getElementById js/document "app")))
