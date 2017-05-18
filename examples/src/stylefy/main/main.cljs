24a158b515dcbc197dcf881a9fbf5a46619841d0
Jari Hanhela <jarihanhela@gmail.com>
2017-05-18 20:35:34 +0300

    Stylefy the example page

--- examples/src/stylefy/main/main.cljs
+++ examples/src/stylefy/main/main.cljs
@@ -2,14 +2,37 @@
   (:require [reagent.core :as r]
             [stylefy.core :as stylefy :refer [style use-style]]))
 
+(def generic-container (style {:padding "25px"
+                               :background-color "#BBBBBB"
+                               :border "1px solid black"}))
+
 (def generic-button (style {:border "1px solid black"
                             :background-color "grey"
+                            :color "white"
+                            :text-align :center
+                            :font-size "1.2em"
                             :padding "5px"
                             :width "150px"
-                            :height "50px"}))
+                            :height "38px"
+                            :margin-right "5px"
+                            :margin-bottom "5px"}))
 
-(defn- hello-world []
+(defn- button []
   [:div (use-style generic-button) "Hello world"])
 
+(defn- button-container []
+  [:div (use-style generic-container)
+   [button]
+   [button]
+   [button]])
+
+(defn- examples []
+  [:div
+   [:h1 "Styled button"]
+   [button]
+
+   [:h1 "Multiple styled buttons in a container"]
+   [button-container]])
+
 (defn start []
-  (r/render hello-world (.getElementById js/document "app")))
+  (r/render examples (.getElementById js/document "app")))
