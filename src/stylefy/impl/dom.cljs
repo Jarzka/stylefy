20a197aab2748d0ad8a5189b79fcaf35609e80e1
Jari Hanhela <jarihanhela@gmail.com>
2017-05-20 13:28:48 +0300

    Remove manual hooks and use requestAnimationFrame to update DOM and <style> when needed

--- src/stylefy/impl/dom.cljs
+++ src/stylefy/impl/dom.cljs
@@ -1,25 +1,28 @@
 (ns stylefy.impl.dom
   (:require [dommy.core :as dommy]
             [garden.core :refer [css]]
-            [reagent.core :as reagent :refer [atom]])
+            [reagent.core :as r :refer [atom]])
   (:require-macros [reagent.ratom :refer [run!]]))
 
 (def ^:private styles-in-use (atom {})) ;; style hash -> props
 (def ^:private stylefy-node-id :#_stylefy-styles_)
+(def ^:private dom-needs-update? (atom false))
 
 (defn- style-by-hash [style-hash]
   (get @styles-in-use style-hash))
 
-(defn- update-styles-in-dom! [styles-in-use]
-  (if-let [node (dommy/sel1 stylefy-node-id)]
-    (let [styles-in-css (map (fn [style-hash]
-                               (::css (style-by-hash style-hash)))
-                             (keys styles-in-use))]
-      (dommy/set-text! node (apply str styles-in-css)))
-    (.error js/console "stylefy is unable to find the required <style> tag!")))
+(defn- update-styles-in-dom! []
+  (when @dom-needs-update?
+    (if-let [node (dommy/sel1 stylefy-node-id)]
+      (let [styles-in-css (map (fn [style-hash]
+                                 (::css (style-by-hash style-hash)))
+                               (keys @styles-in-use))]
+        (dommy/set-text! node (apply str styles-in-css))
+        (reset! dom-needs-update? false))
+      (.error js/console "stylefy is unable to find the required <style> tag!")))
+  (r/next-tick update-styles-in-dom!))
 
-(run!
-  (update-styles-in-dom! @styles-in-use))
+(r/next-tick update-styles-in-dom!)
 
 (defn- save-style! [{:keys [props hash] :as style}]
   (assert props "Unable to save empty style!")
@@ -27,4 +30,5 @@
   (let [style-css (css [(keyword (str "." hash))
                         props])]
     (swap! styles-in-use assoc hash
-           (assoc props ::css style-css))))
+           (assoc props ::css style-css))
+    (reset! dom-needs-update? true)))
