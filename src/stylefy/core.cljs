7d9aaaa3be2506c6426008550366274568f84b4e
Jari Hanhela <jarihanhela@gmail.com>
2017-05-21 22:15:55 +0300

    Fix typos

--- src/stylefy/core.cljs
+++ src/stylefy/core.cljs
@@ -7,16 +7,17 @@
 (defn use-style
   "Defines style for a component.
    Returns a map which contains :class keyword pointing to the given style if the style is found in DOM.
-   If the style has not been added to DOM yet, also returns the given props as inline style, so that
+   If the style has not been added to DOM yet, it also returns the given props as inline style, so that
    the component looks good even if CSS has not been genererated yet.
 
-   The given parameter is a map which contains style properties (as supported by Garden library)
-   There can also be special namespaced keywords along with the style definitions in the same map:
-   ::sub-styles        The content of sub-styles should be a map, in which keys define the name of the sub-style and
+   The given parameter is a map which contains CSS style properties (as supported by Garden library).
+   There can also be special namespaced keywords along with the style definitions:
+   ::sub-styles        The contents of ::sub-styles should be a map,
+                       in which keys define the name of the sub-style and
                        values contain the style properties.
 
    Options is an optional map with the following features:
-   ::with-classes       A vector of class names used with the current style"
+   ::with-classes       A vector of class names used with the current style."
   ([style] (use-style style {}))
   ([style options]
    (impl-styles/use-style! style options)))
