87ab5038a4fa97019efb8295637f2c768d498458
Jari Hanhela <jarihanhela@gmail.com>
2017-05-21 17:12:33 +0300

    Create simple API test

--- test/stylefy/tests/core_test.cljs
+++ test/stylefy/tests/core_test.cljs
@@ -1,5 +1,15 @@
 (ns stylefy.tests.core-test
-  (:require [cljs.test :as test :refer-macros [deftest is]]))
+  (:require [cljs.test :as test :refer-macros [deftest is]]
+            [stylefy.core :as stylefy]))
 
-(deftest dummy
-  (is (= 1 1)))
+(def style-box {:border "1px solid black"
+                :background-color "#FFDDDD"
+                :text-align :center
+                :padding "5px"
+                :width "150px"
+                :height "150px"})
+
+(deftest use-style
+  (let [use-style-response (stylefy/use-style style-box)]
+    (is (string? (:class use-style-response)))
+    (is (= (:style style-box)))))
