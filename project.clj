b75d88d1da137fff007e8bd13903c2365d001510
Jari Hanhela <jarihanhela@gmail.com>
2017-05-22 22:13:26 +0300

    Revert previous changes

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject stylefy "0.5.3"
+(defproject stylefy "0.5.4"
   :description "Library for styling UI components"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.542"]
--- src/stylefy/impl/styles.cljs
+++ src/stylefy/impl/styles.cljs
@@ -23,9 +23,7 @@
   @dom/styles-in-use
 
   (when-not (empty? style)
-    (let [with-classes (:stylefy.core/with-classes options)
-          contains-media-queries? (some? (:stylefy.core/media style))
-          contains-modes? (some? (:stylefy.core/mode style))]
+    (let [with-classes (:stylefy.core/with-classes options)]
 
       (assert (or (nil? with-classes)
                   (and (vector? with-classes)
@@ -41,14 +39,7 @@
         (let [return-map {:class (str/join " " (conj with-classes style-hash))}]
           (if (dom/style-in-dom? style-hash)
             return-map
-            (if (or contains-media-queries? contains-modes?)
-              ;; The style definition has not been added to DOM yet, so return the style props
-              ;; as inline style. Inline style gets replaced soon as the style definition
-              ;; is added to DOM and the component re-renders itself.
-              ;; However, if there are media query or mode definitions, inline styling is proably
-              ;; going to look wrong. Thus, hide the component completely until DOM is ready.
-              (merge return-map {:display "none"})
-              (merge return-map {:style style}))))))))
+            (merge return-map {:style style})))))))
 
 (defn use-sub-style! [style sub-style options]
   (let [resolved-sub-style (get (:stylefy.core/sub-styles style) sub-style)]
