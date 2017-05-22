cfb22bdd9de96b0b51b58b88fdf2eea7f3b412ba
Jari Hanhela <jarihanhela@gmail.com>
2017-05-22 18:31:29 +0300

    Add test for style hashing

--- test/stylefy/runner.cljs
+++ test/stylefy/runner.cljs
@@ -1,5 +1,7 @@
 (ns stylefy.runner
   (:require [doo.runner :refer-macros [doo-tests]]
-            [stylefy.tests.core-test :as core]))
+            [stylefy.tests.core-test :as core]
+            [stylefy.tests.styles-test :as styles]))
 
-(doo-tests 'stylefy.tests.core-test)
+(doo-tests 'stylefy.tests.core-test
+           'stylefy.tests.styles-test)
