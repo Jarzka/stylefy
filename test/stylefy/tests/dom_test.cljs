10c8d84c3615c5292f3ac20854e32360ba05cca0
Jari Hanhela <jarihanhela@gmail.com>
2017-05-22 18:44:15 +0300

    Add test for autoprefix styles

--- test/stylefy/tests/dom_test.cljs
+++ test/stylefy/tests/dom_test.cljs
@@ -5,12 +5,26 @@
             [stylefy.impl.dom :as dom]
             [clojure.string :as str]))
 
-(def style-box {:padding "25px"
+(def simple-style {:padding "25px"
                 :background-color "#BBBBBB"
                 :border "1px solid black"})
 
-(deftest style->css
+(deftest simple-style->css
   (testing "Converting simple style definition to CSS"
-    (is (= (dom/style->css {:props style-box :hash (styles/hash-style style-box)}
+    (is (= (dom/style->css {:props simple-style :hash (styles/hash-style simple-style)}
                            {:pretty-print? false})
            "._stylefy_878532438{padding:25px;background-color:#BBBBBB;border:1px solid black}"))))
+
+(def clickable {:cursor :pointer})
+
+(def autoprefix-style (merge {:border "1px solid black"
+                              :border-radius "5px"
+                            ::stylefy/vendors ["webkit" "moz" "o"]
+                            ::stylefy/auto-prefix #{:border-radius}}
+                             clickable))
+
+(deftest autoprefixed-style->cssK
+  (testing "Converting simple style definition to CSS"
+    (is (= (dom/style->css {:props autoprefix-style :hash (styles/hash-style simple-style)}
+                           {:pretty-print? false})
+           "._stylefy_878532438{border:1px solid black;border-radius:5px;-webkit-border-radius:5px;-moz-border-radius:5px;-o-border-radius:5px;cursor:pointer}" ))))
