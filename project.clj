7d62db51d993c28d6500aff9b422b9a346737bb0
Jari Hanhela <jarihanhela@gmail.com>
2017-05-22 20:05:49 +0300

    Upgrade version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject stylefy "0.4.4"
+(defproject stylefy "0.5"
   :description "Library for styling UI components"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.542"]
