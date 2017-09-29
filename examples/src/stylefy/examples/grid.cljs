33c1e5cd5fa2f30cbbf3bf9f08bd4a8d51d3e661
Jari Hanhela <jarihanhela@gmail.com>
2017-09-29 20:15:22 +0300

    Update grid example

--- examples/src/stylefy/examples/grid.cljs
+++ examples/src/stylefy/examples/grid.cljs
@@ -1,24 +1,31 @@
 (ns stylefy.examples.grid
   (:require [reagent.core :as r]
-            [stylefy.core :as stylefy :refer [use-style use-sub-style]]))
+            [stylefy.core :as stylefy :refer [use-style use-sub-style]]
+            [stylefy.examples.styles :as styles]))
 
 (def grid-style {;; Default style uses Flexbox as fallback
                  :display "flex"
                  :flex-direction "row"
                  :flex-wrap "wrap"
-                 ;; Use CSS Grid if supported by the browser
-                 ;; If the browser does not support feature queries at all, this
+                 :.stylefy/media {{:max-width styles/phone-width}
+                                  {:display "block"}}
+                 ;; Use CSS Grid style if it is supported by the browser
+                 ;; If the browser does not support CSS Grid or feature queries at all, this
                  ;; block is simply ignored.
                  ::stylefy/supports {"display: grid"
                                      {:display "grid"
-                                      :grid-template-columns "1fr 1fr 1fr"}}})
+                                      :grid-template-columns "1fr 1fr 1fr"
+                                      ;; Make CSS Grid responsive
+                                      ::stylefy/media {{:max-width styles/phone-width}
+                                                       {:grid-template-columns "1fr"}}}}})
 
 (defn create-box-style [color]
   {:background-color color
    :width "33.333%"
    :height "200px"
-   ::stylefy/supports {"display: grid" {:width "auto" ;; Managed by grid
-                                        }}})
+   ::stylefy/supports {"display: grid"
+                       ;; Element width is well managed by grid.
+                       {:width "auto"}}})
 
 (defn grid []
   [:div (use-style grid-style)
