828c8459ac0326a2949571138fdb4d3208f90965
Jari Hanhela <jarihanhela@gmail.com>
2017-09-29 20:36:04 +0300

    Fix typo in comment

--- examples/src/stylefy/examples/grid.cljs
+++ examples/src/stylefy/examples/grid.cljs
@@ -9,7 +9,7 @@
                  :flex-wrap "wrap"
                  ::stylefy/media {{:max-width styles/phone-width}
                                   {:display "block"}}
-                 ;; Use CSS Grid style if it is supported by the browser
+                 ;; Use CSS Grid style if it is supported by the browser.
                  ;; If the browser does not support CSS Grid or feature queries at all, this
                  ;; block is simply ignored.
                  ::stylefy/supports {"display: grid"
