a388705585d814b946afd87932af28a3572e4fb1
Jari Hanhela <jarihanhela@gmail.com>
2017-09-29 20:43:46 +0300

    Test vendor prefixes inside media query

--- test/stylefy/tests/dom_test.cljs
+++ test/stylefy/tests/dom_test.cljs
@@ -35,9 +35,15 @@
          "._stylefy_-2110434399{}._stylefy_-2110434399:hover{background-color:#AAAAAA}")))
 
 (def responsive-style {:background-color "red"
-                       ::stylefy/media {{:max-width "500px"} {:background-color "blue"}}})
+                       :border-radius "10px"
+                       ::stylefy/vendors ["webkit" "moz" "o"]
+                       ::stylefy/auto-prefix #{:border-radius}
+                       ::stylefy/media {{:max-width "500px"} {:background-color "blue"
+                                                              :border-radius "5px"
+                                                              ::stylefy/vendors ["webkit" "moz" "o"]
+                                                              ::stylefy/auto-prefix #{:border-radius}}}})
 
 (deftest responsive-style->css
   (is (= (dom/style->css {:props responsive-style :hash (styles/hash-style responsive-style)}
                          {:pretty-print? false})
-         "._stylefy_1443051883{background-color:red}@media(max-width:500px){._stylefy_1443051883{background-color:blue}}")))
+         "._stylefy_-80202655{background-color:red;border-radius:10px;-webkit-border-radius:10px;-moz-border-radius:10px;-o-border-radius:10px}@media(max-width:500px){._stylefy_-80202655{background-color:blue;border-radius:5px;-webkit-border-radius:5px;-moz-border-radius:5px;-o-border-radius:5px}}")))
