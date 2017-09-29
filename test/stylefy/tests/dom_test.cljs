0724d85a0ed32b33bc2f44f53f67ea5f83ed3809
Jari Hanhela <jarihanhela@gmail.com>
2017-09-29 20:48:17 +0300

    Add test for feature query conversion

--- test/stylefy/tests/dom_test.cljs
+++ test/stylefy/tests/dom_test.cljs
@@ -50,3 +50,26 @@
   (is (= (dom/style->css {:props responsive-style :hash (styles/hash-style responsive-style)}
                          {:pretty-print? false})
          "._stylefy_628215496{background-color:red;border-radius:10px;-webkit-border-radius:10px;-moz-border-radius:10px;-o-border-radius:10px}._stylefy_628215496:hover{background-color:white}@media(max-width:500px){._stylefy_628215496{background-color:blue;border-radius:5px;-webkit-border-radius:5px;-moz-border-radius:5px;-o-border-radius:5px}._stylefy_628215496:hover{background-color:grey}}\"")))
+
+
+(def grid-layout-with-fallback {:display "flex"
+                                :flex-direction "row"
+                                :flex-wrap "wrap"
+                                ::stylefy/mode {:hover {:background-color "white"}}
+                                ::stylefy/media {{:max-width "500px"}
+                                                 {:display "block"}}
+                                ::stylefy/supports
+                                {"display: grid"
+                                 {:display "grid"
+                                  :grid-template-columns "1fr 1fr 1fr"
+                                  ;; Make CSS Grid responsive
+                                  ::stylefy/media {{:max-width "500px"}
+                                                   {:grid-template-columns "1fr"
+                                                    ::stylefy/mode {:hover
+                                                                    {:background-color "grey"}}}}}}})
+
+(deftest supports->css
+  (is (= (dom/style->css {:props grid-layout-with-fallback
+                          :hash (styles/hash-style grid-layout-with-fallback)}
+                         {:pretty-print? false})
+         "._stylefy_-978876848{display:flex;flex-direction:row;flex-wrap:wrap}._stylefy_-978876848:hover{background-color:white}@media(max-width:500px){._stylefy_-978876848{display:block}}@supports (display: grid) {._stylefy_-978876848{display:grid;grid-template-columns:1fr 1fr 1fr}@media(max-width:500px){._stylefy_-978876848{grid-template-columns:1fr}._stylefy_-978876848:hover{background-color:grey}}}")))
