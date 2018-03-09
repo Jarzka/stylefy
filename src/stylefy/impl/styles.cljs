d0329ab27583d31501095c6a5910e7ea9ce55664
Jari Hanhela <jarihanhela@gmail.com>
2018-03-09 15:29:57 +0200

    Assert :class type

--- src/stylefy/impl/styles.cljs
+++ src/stylefy/impl/styles.cljs
@@ -32,41 +32,46 @@
 (defn- style-return-value [style style-hash options]
   (let [with-classes (:stylefy.core/with-classes options)
         html-attribuges (utils/filter-props options)
-        html-attributes-class (:class html-attribuges)
+        html-attributes-class (:class html-attribuges)]
 
-        contains-media-queries? (some? (:stylefy.core/media style))
-        contains-feature-queries? (some? (:stylefy.core/supports style))
-        excluded-modes #{:hover}
-        contains-modes-not-excluded? (not (empty?
-                                            (filter (comp not excluded-modes)
-                                                    (keys (:stylefy.core/mode style)))))
-        return-map (merge
-                     html-attribuges
-                     {:class (cond
-                               (vector? html-attributes-class)
-                               (str/join " " (concat html-attributes-class with-classes [style-hash]))
+    (assert (or (nil? html-attributes-class)
+                (string? html-attributes-class)
+                (vector? html-attributes-class))
+            (str "Unsupported :class type (should be nil, string or vector). Got: " (pr-str html-attributes-class)))
 
-                               (string? html-attributes-class)
-                               (str/join " " (concat [html-attributes-class] with-classes [style-hash]))
+    (let [contains-media-queries? (some? (:stylefy.core/media style))
+          contains-feature-queries? (some? (:stylefy.core/supports style))
+          excluded-modes #{:hover}
+          contains-modes-not-excluded? (not (empty?
+                                              (filter (comp not excluded-modes)
+                                                      (keys (:stylefy.core/mode style)))))
+          return-map (merge
+                       html-attribuges
+                       {:class (cond
+                                 (nil? html-attributes-class)
+                                 (str/join " " (concat with-classes [style-hash]))
 
-                               (nil? html-attributes-class)
-                               (str/join " " (concat with-classes [style-hash])))})
-        inline-style (-> style
-                         (utils/filter-props)
-                         (utils/garden-units->to-css))]
-    (if (dom/style-in-dom? style-hash)
-      return-map
-      (if (or contains-media-queries?
-              contains-feature-queries?
-              contains-modes-not-excluded?)
-        ;; The style definition has not been added into the DOM yet, so return the style props
-        ;; as inline style. Inline style gets replaced soon as the style definition
-        ;; is added into the DOM and the component re-renders itself.
-        ;; However, if there are media queries or specific mode definitions, inline styling is probably
-        ;; going to look wrong. Thus, hide the component completely until the DOM is ready.
-        (merge return-map {:style (merge inline-style
-                                         {:visibility "hidden"})})
-        (merge return-map {:style inline-style})))))
+                                 (string? html-attributes-class)
+                                 (str/join " " (concat [html-attributes-class] with-classes [style-hash]))
+
+                                 (vector? html-attributes-class)
+                                 (str/join " " (concat html-attributes-class with-classes [style-hash])))})
+          inline-style (-> style
+                           (utils/filter-props)
+                           (utils/garden-units->to-css))]
+      (if (dom/style-in-dom? style-hash)
+        return-map
+        (if (or contains-media-queries?
+                contains-feature-queries?
+                contains-modes-not-excluded?)
+          ;; The style definition has not been added into the DOM yet, so return the style props
+          ;; as inline style. Inline style gets replaced soon as the style definition
+          ;; is added into the DOM and the component re-renders itself.
+          ;; However, if there are media queries or specific mode definitions, inline styling is probably
+          ;; going to look wrong. Thus, hide the component completely until the DOM is ready.
+          (merge return-map {:style (merge inline-style
+                                           {:visibility "hidden"})})
+          (merge return-map {:style inline-style}))))))
 
 (defn use-style! [style options]
   ;; Deref to make sure components re-render themselves when styles-in-use updates
