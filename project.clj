a03d4ec77f084342519320454bd850ec6269a521
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2018-03-10 10:34:44 +0200

    Move dommy back to main deps

--- project.clj
+++ project.clj
@@ -2,13 +2,13 @@
   :description "Library for styling UI components"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.9.946"]
+                 [prismatic/dommy "1.1.0"]
                  [reagent "0.7.0"]
                  [garden "1.3.2"]]
   :plugins [[lein-cljsbuild "1.1.2"]
             [lein-doo "0.1.7"]
             [lein-codox "0.10.3"]]
-  :profiles {:dev {:dependencies [[prismatic/dommy "1.1.0"]
-                                  [clj-chrome-devtools "20180310"]]}}
+  :profiles {:dev {:dependencies [[clj-chrome-devtools "20180310"]]}}
   :codox {:language :clojurescript
           :output-path "doc"
           :source-paths ["src"]}
