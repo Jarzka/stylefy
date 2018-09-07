263a121c7b0832948f0730b47b24669c93f89eff
Jari Hanhela <jarihanhela@gmail.com>
2018-09-07 15:40:56 +0300

    Simplify test

--- test/stylefy/tests/styles_test.cljs
+++ test/stylefy/tests/styles_test.cljs
@@ -41,11 +41,10 @@
 
 (deftest hash-style-with-custom-prefix
   (let [default-hash (styles/hash-style style-box)
-        custom-prefix-hash (styles/hash-style (assoc style-box ::stylefy/class-prefix "hello-from-cljs-test"))
-        expected-custom-prefix-hash "hello-from-cljs-test_-2018943876"]
+        custom-prefix-hash (styles/hash-style (assoc style-box ::stylefy/class-prefix "hello-from-cljs-test"))]
     (is (= default-hash style-box-expected-hash))
-    (is (= custom-prefix-hash expected-custom-prefix-hash))
-    (is (not= style-box-expected-hash expected-custom-prefix-hash))))
+    (is (= custom-prefix-hash "hello-from-cljs-test_-2018943876"))
+    (is (not= default-hash style-box-expected-hash))))
 
 (deftest hash-sub-styles
   ;; ::sub-styles is only a link to other styles, it
