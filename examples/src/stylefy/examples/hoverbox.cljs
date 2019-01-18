acf0582d0b7f25a66d7921977518857d9aaaa9d4
Jari Hanhela <jarihanhela@gmail.com>
2019-01-18 22:34:47 +0200

    Support vendor prefixes inside inner styles

--- examples/src/stylefy/examples/hoverbox.cljs
+++ examples/src/stylefy/examples/hoverbox.cljs
@@ -2,15 +2,21 @@
   (:require [reagent.core :as r]
             [stylefy.core :as stylefy :refer [use-style use-sub-style]]))
 
-(def hoverbox-style {:width "200px"
-                     :height "200px"
-                     :padding "20px"
-                     :margin-bottom "10px"
-                     :background-color "#55AA55"
-                     ::stylefy/sub-styles {:innerbox {:width "100%"
-                                                      :height "100%"
-                                                      :background-color "#444444"}}
-                     ::stylefy/inner [[:&:hover [:.innerbox {:background-color "#999999"}]]]})
+(def hoverbox-style
+  {:width "200px"
+   :height "200px"
+   :padding "33px"
+   :margin-bottom "10px"
+   :background-color "#55AA55"
+   ::stylefy/class-prefix "seppo"
+   ::stylefy/mode {:hover {:background-color "#99DD99"}}
+   ::stylefy/sub-styles {:innerbox {:width "100%"
+                                    :height "100%"
+                                    :background-color "#444444"}}
+   ::stylefy/inner [[:&:hover [:.innerbox
+                               {:background-color "#999999"}]]
+                    [:&:hover [:.innerbox
+                               [:&:hover {:background-color "#EEEEEE"}]]]]})
 
 (defn hoverbox []
   [:div (use-style hoverbox-style)
