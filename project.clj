4bde4355a1f1d31fbe091c0d144e7fc1339ffd97
Jari Hanhela <jarihanhela@gmail.com>
2020-05-27 18:00:55 +0300

    Mark RC1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject stylefy "2.0.0-beta4"
+(defproject stylefy "2.0.0-rc.1"
   :description "Library for styling UI components"
   :url "https://github.com/Jarzka/stylefy"
   :dependencies [[org.clojure/clojure "1.9.0"]
