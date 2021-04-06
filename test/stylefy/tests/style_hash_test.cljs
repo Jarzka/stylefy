f65b2f3d591bf18f09cecd3bec7527829bd31ccd
Jari Hanhela <jarihanhela@gmail.com>
2021-04-06 16:50:49 +0300

    Formatting

--- test/stylefy/tests/style_hash_test.cljs
+++ test/stylefy/tests/style_hash_test.cljs
@@ -92,8 +92,10 @@
 
 (deftest hash-style-with-garden-units
   ; Hashed garden units should have the same hash as manually written units
-  (is (=  (hashing/hash-style {:width "100px"}) (hashing/hash-style {:width (units/px 100)})))
-  (is (=  (hashing/hash-style {:width "100rem"}) (hashing/hash-style {:width (units/rem 100)})))
+  (is (= (hashing/hash-style {:width "100px"})
+         (hashing/hash-style {:width (units/px 100)})))
+  (is (= (hashing/hash-style {:width "100rem"})
+         (hashing/hash-style {:width (units/rem 100)})))
 
   (is (= (hashing/hash-style h2-style) "_stylefy_154513737"))
   (is (= (hashing/hash-style h3-style) "_stylefy_-1528553558"))
