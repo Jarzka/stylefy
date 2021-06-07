64f25443f2e144c9f506543c472163b6341f208a
Jari Hanhela <jarihanhela@gmail.com>
2021-06-07 17:17:42 +0300

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject stylefy "3.1.0-SNAPSHOT"
+(defproject stylefy "3.1.0"
   :description "Library for styling UI components"
   :url "https://github.com/Jarzka/stylefy"
   :dependencies [[org.clojure/clojure "1.9.0"]
